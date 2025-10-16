91a6e73546de366fc3d55f3bc36de30832d8c999
Zilong Zeng <Zilong3029@gmail.com>
2025-10-16 13:40:20 -0700

    Added genomes and script to run minimap2 and sniffles for long reads

---
+++ run_find_struct_var.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -wd /net/dunham/vol2/Zilong/updating_pipeline_2024/finding_structural_variants_yeast
+#$ -l h_rt=12:00:00
+#$ -l mem_free=32G
+#$ -o /net/dunham/vol2/Zilong/updating_pipeline_2024/outputs/
+#$ -e /net/dunham/vol2/Zilong/updating_pipeline_2024/errors/
+
+module load minimap2/2.26
+module load samtools/1.21
+module load python/3.12.1
+module load sniffles/2.6.3
+mkdir -p results
+
+# Give path to your long read as the first argument to the script
+READS=$1
+REF=genomes/sacCer3.fasta
+
+# strip path and extension from the input filename
+BASENAME=$(basename "$READS" .fastq.gz)
+OUT=results/${BASENAME}.aligned.sorted.bam
+
+minimap2 -t 1 -ax map-ont $REF "$READS" | \
+    samtools view -bS - | \
+    samtools sort -m 8G -@ 1 -o "$OUT"
+
+samtools index "$OUT"
+
+# Use Sniffles2 for detecting structural variants
+cd results
+
+sniffles -i ${BASENAME}.aligned.sorted.bam -v ${BASENAME}.vcf
+
+
+
+
